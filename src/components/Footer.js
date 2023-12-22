951df4b1365540de22a2b6ed7aa6b624dd45b5cc
Ritabrata Paul <95980687+Tatai26@users.noreply.github.com>
2023-12-22 19:32:34 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+export const Footer = () => {
+    const year = new Date().getFullYear();
+  return (
+      
+    <div>
+        <footer>
+        <p>Copyright ⓒ Ritabrata {year}</p>
+
+        </footer>
+    </div>
+  )
+}
